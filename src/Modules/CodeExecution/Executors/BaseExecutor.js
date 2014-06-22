08f1a60bae9036df945eb5fd8bc09930d1fd95b9
dininski <dininski@gmail.com>
2014-06-22 16:42:18 +0300

    Added missing package. Improved error handling. Fixed python dockerfile.

--- src/Modules/CodeExecution/Executors/BaseExecutor.js
+++ src/Modules/CodeExecution/Executors/BaseExecutor.js
@@ -9,7 +9,7 @@ var BaseExecutor = function () {
     this._container = {};
     this._id = '';
     this.timeLimit = 0;
-}
+};
 
 BaseExecutor.prototype = {
     init: function (containerFactory, containerCreateOptions, containerRunOptions) {
@@ -21,9 +21,13 @@ BaseExecutor.prototype = {
     initializeExecution: function (done) {
         var self = this;
         this._containerFactory.createContainer(this._containerCreateOptions, function (err, container) {
-            self._container = container;
-            self._id = container._dockerContainer.id;
-            done();
+            if (err) {
+                done(err);
+            } else {
+                self._container = container;
+                self._id = container._dockerContainer.id;
+                done();
+            }
         });
     },
 
@@ -111,7 +115,7 @@ BaseExecutor.prototype = {
             function removeContainerDlg(data, callback) {
                 self._container.remove(callback);
             }
-        ], function(err) {
+        ], function (err) {
             done(err, result);
         });
     }
