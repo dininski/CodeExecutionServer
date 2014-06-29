0f4629b4369991d0bb11ca5de1ec6985f165daff
Vasil Dininski <dininski@gmail.com>
2014-06-29 16:43:08 +0300

    Added CORS support

--- src/Modules/HttpServer/HttpServer.js
+++ src/Modules/HttpServer/HttpServer.js
@@ -4,18 +4,34 @@ var express = require('express');
 var http = require('http');
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', config.allowedDomains);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    var responseSettings = {
+        "AccessControlAllowOrigin": req.headers.origin,
+        "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
+        "AccessControlAllowMethods": "POST, GET, PUT, DELETE, OPTIONS",
+        "AccessControlAllowCredentials": true
+    };
 
-    next();
+    /**
+     * Headers
+     */
+    res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
+    res.header("Access-Control-Allow-Origin",  responseSettings.AccessControlAllowOrigin);
+    res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
+    res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
+
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
 }
 
 var HttpServer = function (config) {
     this.app = express();
     this.server = http.createServer(this.app);
     this.app.use(express.bodyParser({ keepExtensions: true, uploadDir: config.uploadDir }));
-    this.app.use(allowCrossDomain);
+    this.app.all(allowCrossDomain);
 };
 
 HttpServer.prototype = {
