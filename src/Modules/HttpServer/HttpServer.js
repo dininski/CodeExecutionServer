eea924e44f30c5119c5ab5c69d92f3944dee81b7
Vasil Dininski <dininski@gmail.com>
2014-06-29 16:34:14 +0300

    Added CORS support

--- src/Modules/HttpServer/HttpServer.js
+++ src/Modules/HttpServer/HttpServer.js
@@ -3,10 +3,19 @@
 var express = require('express');
 var http = require('http');
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', config.allowedDomains);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
 var HttpServer = function (config) {
     this.app = express();
     this.server = http.createServer(this.app);
     this.app.use(express.bodyParser({ keepExtensions: true, uploadDir: config.uploadDir }));
+    this.app.use(allowCrossDomain);
 };
 
 HttpServer.prototype = {
